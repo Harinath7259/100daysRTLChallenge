e450c3ea465ab34fd05507000f90e93476ede1b1
Udatha Harinath <uhnath99@gmail.com>
2024-07-02 15:19:45 +0530

    Create twos_complementor.v

---
+++ day 8 : 2's complemntor/twos_complementor.v
@@ -0,0 +1,12 @@
+
+module complementor_2s( input [3:0] a,output [3:0] b);
+ 
+ assign b[0] = a[0];
+ 
+ assign b[1] = a[1] ^ b[0];
+ 
+ assign b[2] = a[2]&~a[1]&~a[0] | ~a[2]&a[0] | ~a[2]&a[1];
+ 
+ assign b[3] = ~a[3]&(a[0] | a[1] | a[2]) | a[3]&~a[2]&~a[1]&~a[0];
+ 
+endmodule
