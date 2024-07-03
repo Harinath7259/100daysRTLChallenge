c126bacf14d1b713093d6cccd7caf75199e8d368
Udatha Harinath <uhnath99@gmail.com>
2024-07-03 17:03:20 +0530

    Update t_ff.v

--- day 29 : 4 bit Asynchronous Up counter/t_ff.v
+++ day 29 : 4 bit Asynchronous Up counter/t_ff.v
@@ -1,1 +1,23 @@
 
+module t_flipflop(input t,clk,rst, output reg qn,output qbar);
+
+always@(posedge clk or posedge rst) begin
+
+ if(rst) begin
+  qn = 0 ;
+ end
+ 
+ else begin
+ 
+  case(t) 
+   1'b0 : qn = qn ;
+   1'b1 : qn = ~qn ;
+  endcase
+  
+  end
+  
+  end
+ assign qbar = ~qn ;
+  
+endmodule
+
